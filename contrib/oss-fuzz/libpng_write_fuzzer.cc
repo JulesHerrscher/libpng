56709ccc652db089ec1e837674868d1634a01bf4
Jules <jules.herrscher@epfl.ch>
2025-05-09 01:12:15 +0200

    attempt to fix the harness by taking inspiration from the existing harness

--- contrib/oss-fuzz/libpng_write_fuzzer.cc
+++ contrib/oss-fuzz/libpng_write_fuzzer.cc
@@ -9,14 +9,23 @@
 #include "png.h"
 
 // Write-specific cleanup
-#define PNG_WRITE_CLEANUP \
-  if (png_handler.png_ptr) { \
+#define PNG_CLEANUP \
+  if(png_handler.png_ptr) \
+  { \
     if (png_handler.row_ptr) \
       png_free(png_handler.png_ptr, png_handler.row_ptr); \
     if (png_handler.end_info_ptr) \
-      png_destroy_write_struct(&png_handler.png_ptr, &png_handler.info_ptr); \
+      png_destroy_read_struct(&png_handler.png_ptr, &png_handler.info_ptr,\
+        &png_handler.end_info_ptr); \
+    else if (png_handler.info_ptr) \
+      png_destroy_read_struct(&png_handler.png_ptr, &png_handler.info_ptr,\
+        nullptr); \
     else \
-      png_destroy_write_struct(&png_handler.png_ptr, nullptr); \
+      png_destroy_read_struct(&png_handler.png_ptr, nullptr, nullptr); \
+    png_handler.png_ptr = nullptr; \
+    png_handler.row_ptr = nullptr; \
+    png_handler.info_ptr = nullptr; \
+    png_handler.end_info_ptr = nullptr; \
   }
 
 struct BufState {
@@ -25,14 +34,22 @@ struct BufState {
   size_t size;
 };
 
-struct PngWriteHandler {
-  png_structp png_ptr = nullptr;
+struct PngObjectHandler {
   png_infop info_ptr = nullptr;
-  png_bytep row_ptr = nullptr;
+  png_structp png_ptr = nullptr;
+  png_infop end_info_ptr = nullptr;
+  png_voidp row_ptr = nullptr;
   BufState* buf_state = nullptr;
 
-  ~PngWriteHandler() {
-    PNG_WRITE_CLEANUP
+  ~PngObjectHandler() {
+    if (row_ptr)
+      png_free(png_ptr, row_ptr);
+    if (end_info_ptr)
+      png_destroy_read_struct(&png_ptr, &info_ptr, &end_info_ptr);
+    else if (info_ptr)
+      png_destroy_read_struct(&png_ptr, &info_ptr, nullptr);
+    else
+      png_destroy_read_struct(&png_ptr, nullptr, nullptr);
     delete buf_state;
   }
 };
@@ -52,7 +69,34 @@ void user_flush_data(png_structp) {}
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   if (size < 32) return 0;  // Minimal input size check
 
-  PngWriteHandler png_handler;
+  PngObjectHandler png_handler;
+  png_handler.png_ptr = nullptr;
+  png_handler.row_ptr = nullptr;
+  png_handler.info_ptr = nullptr;
+  png_handler.end_info_ptr = nullptr;
+
+  png_handler.png_ptr = png_create_read_struct
+    (PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+  if (!png_handler.png_ptr) {
+    return 0;
+  }
+
+  png_handler.info_ptr = png_create_info_struct(png_handler.png_ptr);
+  if (!png_handler.info_ptr) {
+    PNG_CLEANUP
+    return 0;
+  }
+
+  png_handler.end_info_ptr = png_create_info_struct(png_handler.png_ptr);
+  if (!png_handler.end_info_ptr) {
+    PNG_CLEANUP
+    return 0;
+  }
+
+  // Use a custom allocator that fails for large allocations to avoid OOM.
+  png_set_mem_fn(png_handler.png_ptr, nullptr, limited_malloc, default_free);
+
+  png_set_crc_action(png_handler.png_ptr, PNG_CRC_QUIET_USE, PNG_CRC_QUIET_USE);
   
   try {
     // Initialize write structures
