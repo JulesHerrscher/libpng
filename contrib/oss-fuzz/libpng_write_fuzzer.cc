c1bfd2dd0b465125178a046f41f362f88f5355d4
Jules <jules.herrscher@epfl.ch>
2025-05-10 17:07:31 +0200

    added functions and starting to build the harness using parts of the existing harness

--- contrib/oss-fuzz/libpng_write_fuzzer.cc
+++ contrib/oss-fuzz/libpng_write_fuzzer.cc
@@ -52,10 +52,47 @@ struct PngObjectHandler {
   }
 };
 
+void* limited_malloc(png_structp, png_alloc_size_t size) {
+  if (size > 8000000)
+    return nullptr;
+
+  return malloc(size);
+}
+
+void defualt_free(png_structp, png_voidp ptr) {
+  return free(ptr);
+}
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
  if (size < 8) {
     return 0;
   }
+
+  PngObjectHandler png_handler;
+  png_handler.png_ptr = nullptr;
+  png_handler.row_ptr = nullptr;
+  png_handler.info_ptr = nullptr;
+  png_handler.end_info_ptr = nullptr;
+
+  png_handler.png_ptr = png_create_read_struct
+    (PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+  if (!png_handler.png_ptr) {
+    return 0;
+  }
+
+  png_handler.info_ptr = png_create_info_struct(png_handler.png_ptr);
+  if (!png_handler.info_ptr) {
+    PNG_CLEANUP
+    return 0;
+  }
+
+  png_handler.end_info_ptr = png_create_info_struct(png_handler.png_ptr);
+  if (!png_handler.end_info_ptr) {
+    PNG_CLEANUP
+    return 0;
+  }
+
+  png_set_mem_fn(png_handler.png_ptr, nullptr, limited_malloc, default_free);
+
   return 0;
 }
