865690cd6d3182a336553e2315f592abcf6370ab
JulesHerrscher <jules.herrscher@epfl.ch>
2025-05-12 21:45:56 +0000

     changed -lFuzzingEngine to  in the build.sh to try to link correctly

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -46,7 +46,7 @@ $CXX $CXXFLAGS -std=c++11 -I. \
 $CXX $CXXFLAGS -std=c++11 -I. \
      $SRC/libpng/contrib/oss-fuzz/libpng_write_fuzzer.cc \
      -o $OUT/libpng_write_fuzzer \
-     -lFuzzingEngine .libs/libpng16.a -lz
+     $LIB_FUZZING_ENGINE .libs/libpng16.a -lz
 
 # add seed corpus.
 find $SRC/libpng -name "*.png" | grep -v crashers | \
