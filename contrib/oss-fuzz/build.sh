ed91505357e3c3b205f9d5eb2df23b9e57d2360a
JulesHerrscher <jules.herrscher@epfl.ch>
2025-05-12 12:47:35 +0200

    modified build.sh to try to fuzz without initial seeds

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -49,8 +49,8 @@ $CXX $CXXFLAGS -std=c++11 -I. \
      -lFuzzingEngine .libs/libpng16.a -lz
 
 # add seed corpus.
-find $SRC/libpng -name "*.png" | grep -v crashers | \
-     xargs zip $OUT/libpng_read_fuzzer_seed_corpus.zip
+#find $SRC/libpng -name "*.png" | grep -v crashers | \
+#     xargs zip $OUT/libpng_read_fuzzer_seed_corpus.zip
 
 # add the same seed corpus for the write fuzzer.
 find $SRC/libpng -name "*.png" | grep -v crashers | \
